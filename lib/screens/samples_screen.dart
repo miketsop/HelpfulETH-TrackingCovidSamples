10dc86dd99d61538453991be45473bcdf0864476
Michail Tsopelas <mtsopelas@gmail.com>
2020-05-13 16:53:31 +0200

    Updated samples screen in case bottom navigation bar is used!

--- lib/screens/samples_screen.dart
+++ lib/screens/samples_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
 import '../widgets/sample.dart';
-import '../widgets/main_drawer.dart';
 
 class SamplesScreen extends StatefulWidget {
   static const routeName = '/samples-screen';
@@ -11,40 +9,28 @@ class SamplesScreen extends StatefulWidget {
 }
 
 class _SamplesScreenState extends State<SamplesScreen> {
-
-  void _onBackPressed(){
-    Navigator.of(context).pop();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(icon: Icon(Icons.arrow_back), onPressed: _onBackPressed),
-          title: const Text('List of COVID samples'),
-        ),
-        drawer: MainDrawer(),
-        body: StreamBuilder(
-          stream: Firestore.instance
-              .collection('samples')
-              .orderBy('timestamp', descending: true)
-              .snapshots(),
-          builder: (ctx, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            final samples = snapshot.data.documents;
-            if (samples.length == 0){
-              return Center(child: const Text('No samples added yet!'));
-            }
-            return ListView.builder(
-              itemCount: samples.length,
-              itemBuilder: (ctx, indx) => Sample(samples[indx].data),
-            );
-          },
-        ),
+    return StreamBuilder(
+      stream: Firestore.instance
+          .collection('samples')
+          .orderBy('timestamp', descending: true)
+          .snapshots(),
+      builder: (ctx, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        final samples = snapshot.data.documents;
+        if (samples.length == 0) {
+          return Center(child: const Text('No samples added yet!'));
+        }
+        return ListView.builder(
+          itemCount: samples.length,
+          itemBuilder: (ctx, indx) => Sample(samples[indx].data),
+        );
+      },
     );
   }
 }
